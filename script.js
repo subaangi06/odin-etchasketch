7b26bf1f2e3632b0554358953f64a1a544043716
subaangi06 <balaraajsubaangi@gmail.com>
2026-01-27 13:41:46 +0530

    Create grid with hardcoded value of 16 squares per column/row

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector("#container");
+const CONTAINERWIDTH = 650;
+let containerSquarePerSide = 16;
+let colourSquareWidth = CONTAINERWIDTH/containerSquarePerSide;
+
+
+for (let i = 0; i<containerSquarePerSide*containerSquarePerSide; i++){
+    const colourSquare = document.createElement("div");
+    colourSquare.classList.add("colourSquare");
+    //percentage width of total width (100%)
+    const percentSize = 100/containerSquarePerSide;
+    //flex-grow(1):allows squares to grow to fill gaps, flex-shrink(0):prevents squares from squishing of row is full, flex-basis(percentsize):sets base width as a percentage
+    //using percentage is more reliable than hardcoding pixel division, because percentage will add up exactly to 100, preventing overflowing errors
+    //percentage tells the browser to fit exactly 16 squares take up 100% of the width, despite rounding errors. 
+    //height: set in pixels to match mathematical width to ensure perfect square 
+    //height can be hardcoded as they arent trying to wrap around, like width getting wrapped to the next row. 
+    colourSquare.setAttribute("style", `flex: 1 0 ${percentSize}%; height:${colourSquareWidth}px;`)
+    container.appendChild(colourSquare);
+
+}
+
